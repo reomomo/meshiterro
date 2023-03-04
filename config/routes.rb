a6639b5858dc149c52a05c098438ca2117f0bdf0
EC2 Default User <ec2-user@ip-172-31-9-74.ap-northeast-1.compute.internal>
2023-03-04 14:17:57 +0000

    [Add]アソシエーションの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :postimages, only: [:new, :index, :show]
   devise_for :users
   get 'homes/about' => 'homes#about', as: 'about'
   root to: 'homes#top'
